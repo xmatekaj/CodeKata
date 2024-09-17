90eda8d275d7664bc04c6c157fb7b584e31c2485
Mateusz <matekaj@proton.me>
2024-09-17 18:37:43 +0200

    LeetCode

---
+++ CodeKataCs/LeetCode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKataCs
+{
+    internal static class LeetCode
+    {
+        // 884. Uncommon Words from Two Sentences
+        // https://leetcode.com/problems/uncommon-words-from-two-sentences/
+        public static string[] UncommonFromSentences(string s1, string s2)
+        {
+            string[] list1 = s1.Split(' ');
+            string[] list2 = s2.Split(' ');
+            var res = new List<string>();
+            foreach (var s in list1)
+            {
+                int numOfElements = list1.Where(x => x.Equals(s)).Select(x => x).Count();
+                if (numOfElements == 1 && !list2.Contains(s)) res.Add(s);
+            }
+            foreach (var s in list2)
+            {
+                int numOfElements = list2.Where(x => x.Equals(s)).Select(x => x).Count();
+                if (numOfElements == 1 && !list1.Contains(s)) res.Add(s);
+            }
+            return res.ToArray();
+        }
+    }
+}
